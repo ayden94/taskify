e5117d7c82c1bf38dd009d159f38ad0d74af1308
ayden94 <144667387+ayden94@users.noreply.github.com>
2024-09-05 19:56:01 +0900

    feat : 테스트 계정 로그인 기능 추가

--- pages/components/IndexHeader.tsx
+++ pages/components/IndexHeader.tsx
@@ -2,12 +2,16 @@ import clsx from "clsx";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./IndexHeader.module.css";
+import sender from "@/apis/sender";
+import { useRouter } from "next/router";
 
 interface IndexHeaderProps {
   color: "white" | "black";
 }
 
 const IndexHeader = ({ color = "white" }: IndexHeaderProps) => {
+  const router = useRouter();
+
   return (
     <>
       <header
@@ -28,6 +32,28 @@ const IndexHeader = ({ color = "white" }: IndexHeaderProps) => {
             />
           </picture>
         </Link>
+        <button
+          type="button"
+          onClick={async (e) => {
+            e.preventDefault();
+
+            const signData = {
+              email: "test@taskify.com",
+              password: "test0486",
+            };
+
+            const signinRes = await sender.post({ path: "signin", data: signData });
+
+            if (signinRes.status === 201) {
+              document.cookie = `accessToken=${signinRes.data.accessToken}`;
+              router.push("/mydashboard");
+              return;
+            }
+          }}
+          className={styles.test_link}
+        >
+          테스트 로그인
+        </button>
         <Link className={styles.login__link} href="/signin">
           로그인
         </Link>
